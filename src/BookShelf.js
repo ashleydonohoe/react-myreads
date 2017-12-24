7c0720361bea45e1832088ffde3d2a639820b1ec
ashleydonohoe <ashleyrdonohoe@gmail.com>
2017-12-23 19:16:50 -0500

    more refactoring

---
+++ src/BookShelf.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Book from './Book';
+
+class BookShelf extends Component {
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{ this.props.title }</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            <Book />
+            <Book />
+            <Book />
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BookShelf;
