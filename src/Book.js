031a15bd553bd141e3ca2e512bb6d1c028ada6b2
ashleydonohoe <ashleyrdonohoe@gmail.com>
2017-12-25 17:36:00 -0500

    updating bug with shelf on search page

--- src/Book.js
+++ src/Book.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 const Book = ({book, onShelfChange}) => {
   const { title, subtitle, authors, imageLinks, shelf } = book;
+  const currentShelf = shelf ? shelf : "none";
   const fullTitle = subtitle ? `${title} - ${subtitle}` : title;
   const fullAuthors = authors ? authors.join(', ') : '';
   const bookImageURL = imageLinks.thumbnail ? imageLinks.thumbnail : '';
-  
+
   return (
     <li>
       <div className="book">
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${bookImageURL})` }}></div>
           <div className="book-shelf-changer">
-            <select onChange={e => onShelfChange(book, e.target.value)} value={shelf}>
+            <select onChange={e => onShelfChange(book, e.target.value)} value={currentShelf}>
               <option value="none" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
