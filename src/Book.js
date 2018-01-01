3ad82aebcfbf8d3da145a9f5916f2fe4fb00436f
ashleydonohoe <ashleyrdonohoe@gmail.com>
2017-12-31 22:03:35 -0500

    last commit

--- src/Book.js
+++ src/Book.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Book = ({book, onShelfChange}) => {
   const { title, subtitle, authors, imageLinks, shelf } = book;
-  const currentShelf = shelf ? shelf : "none";
+  const currentShelf = shelf ? shelf : "no-shelf";
   const fullTitle = subtitle ? `${title} - ${subtitle}` : title;
   const fullAuthors = authors ? authors.join(', ') : '';
-  const bookImageURL = imageLinks.thumbnail ? imageLinks.thumbnail : '';
+  // Used bookcover placeholder suggested by reviewer when no thumbnail is available
+  const bookImageURL = imageLinks ? imageLinks.thumbnail : 'http://via.placeholder.com/128x193?text=No%20Cover';
 
   return (
     <li>
@@ -14,7 +15,7 @@ const Book = ({book, onShelfChange}) => {
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${bookImageURL})` }}></div>
           <div className="book-shelf-changer">
             <select onChange={e => onShelfChange(book, e.target.value)} value={currentShelf}>
-              <option value="none" disabled>Move to...</option>
+              <option value="no-shelf" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
               <option value="read">Read</option>
--- src/Search.js
+++ src/Search.js
@@ -16,23 +16,40 @@ class Search extends Component {
       queryString
     });
 
-    // Get search results if there's currently a queryString
-    if(queryString) {
-      BooksAPI.search(queryString).then((books) => {
-        // If books has at least 1 book in it, update state with these new books
-        if(books.length > 0) {
-          this.setState({
-            bookResults: books,
-            error: false
-          });
-          // if there is no books, there's an error somewhere. Clear books list
-        } else {
-          this.setState({
-            error: true,
-            bookResults: []
-          });
-        }
+    // Empty book results and don't search for empty query
+    if(queryString.length === 0) {
+      console.log("Query too short");
+      this.setState({
+        bookResults: []
       });
+    } else {
+      // Get search results if there's currently a queryString
+      if(queryString) {
+        BooksAPI.search(queryString).then((newBooks) => {
+          // If books has at least 1 book in it, update state with these new books
+          if(newBooks.length > 0) {
+            // Check for existing books to show the proper state of the bookshelf
+            newBooks.forEach((newBook, index) => {
+              // Find if book is in the existing books and update shelf if so
+              this.props.books.forEach((oldBook) => {
+                if(newBook.id === oldBook.id) {
+                  newBooks[index].shelf = oldBook.shelf;
+                }
+              });
+            });
+            this.setState({
+              bookResults: newBooks,
+              error: false
+            });
+            // if there is no books, there's an error somewhere. Clear books list
+          } else {
+            this.setState({
+              error: true,
+              bookResults: []
+            });
+          }
+        });
+      }
     }
   }
 
@@ -40,6 +57,8 @@ class Search extends Component {
     // Render the bookshelf if there are books in the array
     // Render no results message otherwise
     const { bookResults } = this.state;
+    const existingBooks = this.props.books;
+    console.log(existingBooks);
     const content = bookResults.length > 0 ? <BookShelf books={bookResults} title="Book results" onShelfChange={this.props.onShelfChange} /> : "No Results yet";
     return content;
   }
